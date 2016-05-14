a8fc7e2f6bc5c2d808580d6e5d846d7b942f39fb
A Russo <abmmrusso@users.noreply.github.com>
2016-05-14 16:59:22 +0100

    Setup and requirements

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+class PyTest(TestCommand):
+    def initialize_options(self):
+	TestCommand.initialize_options(self)
+	self.test_args = []
+
+    def run_tests(self):
+        import pytest
+        import sys
+        errcode = pytest.main(self.test_args)
+        sys.exit(errcode)
+
+setup(
+	name = "Destiny Lore Ebook Generator",
+	description = "Destiny Lore ebook generation tool from API accessible info.",
+	version = "0.1",
+	license = "MIT",
+	author_email = "antoniobmmrusso@users.noreply.github.com",
+	author = "Antonio Russo",
+	keywords = "destiny bungie lore ebook",
+	packages = find_packages(),
+	cmdclass={"test": PyTest},
+	tests_require=[ 'pytest', 'mock'],
+)
